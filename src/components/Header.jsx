2cdc1b76b8d2fd6dc3eaddc7af5a1e7a3cd173eb
León <56575005+LeonCostandopulo@users.noreply.github.com>
2025-03-18 17:51:57 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header className='header'>
-      <img className='header-logo' src="../logo-icon.webp" alt="Logo de la empresa" />
+      <img className='header-logo' src="/logo-icon.webp" alt="Logo de la empresa" />
       <nav className='header-nav'>
         <ul className='header-nav-list'>
           <li className='header-nav-list-item'>Packs</li>
