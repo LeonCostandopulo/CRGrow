ae986d79d1410c8a92a42e73a40757d2859c652a
leon <nashe@god.com>
2025-03-25 15:52:44 -0300

    LINKS FIXED

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export function Footer() {
   const footerText = "Desarrollado por "
   return (
     <footer className="footer">
-      <p>{footerText} <a href="https://www.linkedin.com/in/leoncostandopulo/?" target="_blank">León Costandopulo</a> </p>
+      <p>{footerText} <a href="https://www.linkedin.com/in/le%C3%B3n-costandopulo-57b3a2236/?" target="_blank">León Costandopulo</a> </p>
     </footer>
   )
 }
--- src/components/KitsSection.jsx
+++ src/components/KitsSection.jsx
@@ -3,7 +3,7 @@ import { Pack } from './Pack';
 
 export function KitsSection() {
   return(
-    <section className='kits'>
+    <section id="packs" className='kits'>
       <Pack number="1">Kit (top 2)</Pack> 
       <Pack number="2">¡Kit en OFERTA! -64%</Pack> 
       <Pack number="3">Kit (top 3)</Pack> 
